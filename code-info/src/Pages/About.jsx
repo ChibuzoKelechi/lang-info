e21cc22972268bb20941369c5dac47f8ae5bf7ac
KelechiCV <narutokelechi@gmail.com>
2023-09-30 13:52:18 +0100

    Removed unnecessary parts

--- code-info/src/Pages/About.jsx
+++ code-info/src/Pages/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
              initial={{ opacity: 0 }}
              whileInView={{ opacity: 1 }}
             >
-             <h1 className='bg-gradient-to-r from-blue-400 to-green-400 bg-clip-text text-4xl font-extrabold text-transparent'> Everything you need to know</h1>
+             <h1 className='bg-gradient-to-r from-sky-400 to-indigo-400 bg-clip-text text-4xl font-extrabold text-transparent'> Everything you need to know</h1>
             </motion.article>
         </section>
      );
